4f241a7bef5a9375e6baf17e95533e30c8e0e785
gabs <j4m41c4x4i20@gmail.com>
2022-08-19 16:15:15 -0300

    falta evolução q ta um cu pra fazer

--- reactapp/src/components/Specified/Specified.jsx
+++ reactapp/src/components/Specified/Specified.jsx
@@ -5,7 +5,24 @@ import "./Specified.css";
 
 export function Specified() {
   const params = useParams();
-  const [status, setStatus] = useState([]);
+  const [pokeInfo, setPokeInfo] = useState({});
+
+  useEffect(() => {
+    async function getEvolve() {
+      const resultado = await fetch(
+        "https://pokeapi.co/api/v2/evolution-chain/" + params.id,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      let evolutions = resultado.json();
+      console.log(evolutions);
+    }
+    getEvolve();
+  }, []);
 
   useEffect(() => {
     async function info() {
@@ -19,8 +36,8 @@ export function Specified() {
         }
       );
       let infos = await resultado.json();
-      console.log(infos);
-      setStatus(Object.keys(infos));
+      // console.log(JSON.stringify(infos, null, 2));
+      setPokeInfo(infos);
     }
     info();
   }, []);
@@ -32,12 +49,52 @@ export function Specified() {
         height: "400px",
       }}
     >
-      {status.map((status, index) => {
-        return (
-          <div key={index}>
-            <p>{status}</p>
-          </div>
-        );
+      <img
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+          params.id + ".png"
+        }`}
+      ></img>
+      {Object.keys(pokeInfo).map((pokeInfoName, index) => {
+        if (
+          !["forms", "weight", "stats", "types", "species"].includes(
+            pokeInfoName
+          )
+        ) {
+          return null;
+        }
+        const pokeInfoStatus = pokeInfo[pokeInfoName];
+        if (pokeInfoName == "forms") {
+          return (
+            <div className="data-box">
+              <h3>Forma</h3>
+              {pokeInfoStatus[0].name}
+            </div>
+          );
+        } else if (pokeInfoName === "stats") {
+          return (
+            <div className="data-box">
+              <h3>Status</h3>
+              {pokeInfoStatus.map((stats) => (
+                <>
+                  <div className="row">
+                    <b>{stats.stat.name}: </b>
+                    {stats.base_stat}
+                  </div>
+                </>
+              ))}
+            </div>
+          );
+        } else {
+          return (
+            <div className="data-box">
+              <h3>{pokeInfoName}: </h3>
+              {pokeInfoName == "types" &&
+                pokeInfoStatus[pokeInfoStatus.length - 1].type.name}
+              {pokeInfoName === "species" && pokeInfoStatus.name}
+              {pokeInfoName === "weight" && pokeInfoStatus}
+            </div>
+          );
+        }
       })}
     </div>
   );
